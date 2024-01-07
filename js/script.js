dede2a0b7e480f078d330ccd6cfa2320adb13c6d
janinocencio <janinocencio@gmail.com>
2024-01-07 15:51:03 +1100

    Add success functionality for first name

--- js/script.js
+++ js/script.js
@@ -41,7 +41,50 @@ submitBtn.addEventListener('click', function(event) {
             input.previousElementSibling.style.transition = '0.3s ease';
             input.nextElementSibling.textContent = "*This field is required.";
             input.nextElementSibling.style.color = 'var(--color-1)';
+        } else {
+            switch(input.id) {
+                case 'name_first':
+                    const nameRegex = /^[a-zA-Z]+( [a-zA-Z]+)?$/
+                    if (nameRegex.test(input.value.trim())) {
+                        input.style.border = '0.2rem solid var(--color-4)'
+                        input.style.transition = '0.1s'
+                        input.previousElementSibling.style.background = 'var(--color-4)';
+                        input.previousElementSibling.style.color = 'black';
+                        input.previousElementSibling.style.transform = 'translate(0.5rem, -1.25rem)';
+                        input.previousElementSibling.style.fontSize = '1rem';
+                        input.previousElementSibling.style.padding = '0 1rem';
+                        input.previousElementSibling.style.transition = '0.3s ease';
+                        input.nextElementSibling.textContent = '';
+                    } else {
+                        input.style.border = '0.2rem solid var(--color-1)'
+                        input.style.transition = '0.1s'
+                        input.previousElementSibling.style.background = 'var(--color-1)';
+                        input.previousElementSibling.style.color = 'black';
+                        input.previousElementSibling.style.transform = 'translate(0.5rem, -1.25rem)';
+                        input.previousElementSibling.style.fontSize = '1rem';
+                        input.previousElementSibling.style.padding = '0 1rem';
+                        input.previousElementSibling.style.transition = '0.3s ease';
+                        input.nextElementSibling.textContent = "*Please input a valid name.";
+                        input.nextElementSibling.style.color = 'var(--color-1)';
+                    }
+                    break;
+                case 'name_last':
+                    console.log('test2');
+                    break;
+                case 'email':
+                    console.log('email');
+                    break;
+                case 'mobile':
+                    console.log('mobile');
+                    break;
+                case 'password_orig':
+                    console.log('passOrig');
+                    break;
+                case 'password_confirm':
+                    console.log('passwordConfirm');
+                    break;
+            }
         }
-        
+
     })
 });
